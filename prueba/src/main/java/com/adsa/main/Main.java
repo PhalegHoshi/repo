449574c10130ee661e846a64d9191bc3706bfde1
L-Zero <L-Zero@LAPTOP-23QDAB9B>
2020-12-05 13:19:52 -0600

    Carga inicial

---
+++ prueba/src/main/java/com/adsa/main/Main.java
@@ -0,0 +1,20 @@
+package com.adsa.main;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Resource recurso = new ClassPathResource("springconfig.xml");
+		BeanFactory factory = new XmlBeanFactory(recurso);
+		
+		Persona persona = (Persona)factory.getBean("persona");
+		
+		persona.mostrarEstudianteActual();
+
+	}
+
+}
---
+++ prueba/src/main/java/com/adsa/main/Persona.java
@@ -0,0 +1,24 @@
+package com.adsa.main;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public void mostrarEstudianteActual() {
+		System.out.println(nombre+" "+apellido+", es la persona actual");
+	}
+	
+}
